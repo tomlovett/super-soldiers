b111fdfcb6fc0a0835ab7a7f4863e182a8f9496f
Tom Lovett <tomlovett6@gmail.com>
2019-05-15 12:15:44 -0400

    Seed data

--- rails-api/db/seeds.rb
+++ rails-api/db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(name: 'Commander', email: 'c@work.com', password: 'pass', password_confirmation: 'pass')
+
+bart = Soldier.create(first_name: 'Bart', last_name: 'Chrysler', nationality: 'USA', gender: 'm', is_alive: true, user: user)
+lee = Soldier.create(first_name: 'Lee', last_name: 'Syatt', nationality: 'Israel', gender: 'm', is_alive: true, user: user)
+crystal = Soldier.create(first_name: 'Christina', last_name: 'P', nationality: 'Poland', gender: 'f', is_alive: true, user: user)
+tommy = Soldier.create(first_name: 'Tom', last_name: 'Segura', nationality: 'Peru', gender: 'm', is_alive: true, user: user)
+uncle_joey = Soldier.create(first_name: 'Joey', last_name: 'Diaz' , nationality: 'Cuba', gender: 'm', is_alive: true, user: user)
+
+cool_guy = Mission.create(name: 'Cool Guy')
+cool_guy.soldiers << bart
+cool_guy.soldiers << lee
+cool_guy.soldiers << crystal
+cool_guy.soldiers << tommy
+
+try_it_out = Mission.create(name: 'Try It Out')
+try_it_out.soldiers << tommy
+try_it_out.soldiers << crystal
+try_it_out.soldiers << uncle_joey
+try_it_out.soldiers << bart
