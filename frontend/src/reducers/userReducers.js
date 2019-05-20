76f787d216c9a9f67990a12fdf168ac26c50084d
Tom Lovett <tomlovett6@gmail.com>
2019-05-20 12:22:42 -0400

    Store default token

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,6 +1,9 @@
 import * as types from '../constants/actionTypes';
 
-export const user = (state = {}, action) => {
+const hasToken = { token: "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1NTg0NTQ3OTJ9.uUYv6iAG3VK5cfDE3SQRJ0squUONoxeG7vVkMzgrZfw"}
+
+// export const user = (state = {}, action) => {
+export const user = (state = hasToken, action) => {
   switch (action.type) {
     case types.AUTH_SUCCESS:
       return Object.assign({}, {token: action.token}, state);
