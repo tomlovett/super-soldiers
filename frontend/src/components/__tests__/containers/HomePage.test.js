8d79dc7f3460a78fcb4d94bcf56346590b223535
Tom Lovett <tomlovett6@gmail.com>
2019-05-29 15:55:44 -0400

    HomePage tests cleanup

--- frontend/src/components/__tests__/containers/HomePage.test.js
+++ frontend/src/components/__tests__/containers/HomePage.test.js
@@ -3,38 +3,43 @@ import { shallow } from 'enzyme';
 import sinon from 'sinon';
 import { HomePage } from '../../containers/HomePage';
 
+const authenticate = jest.fn().mockName('authenticate');
+const querySelf = jest.fn().mockImplementation(() => Promise.resolve());
+
 const actions = {
-  authenticate: jest.fn(),
-  querySelf: jest.fn().mockImplementation(() => Promise.resolve())
+  authenticate,
+  querySelf
 };
 
 const userWithToken = { token: 'authToken' };
 
 describe('<HomePage />', () => {
   it('renders', () => {
-    const wrapper = shallow(<HomePage actions={actions} user={{}} history={{}} />);
+    const wrapper = shallow(<HomePage actions={{}} user={{}} history={{}} />);
 
     expect(wrapper.find('HomePage').exists()).toBeTruthy;
   });
 
   it('renders a Login form and a Register form', () => {
-    const wrapper = shallow(<HomePage actions={actions} user={{}} history={{}} />);
+    const wrapper = shallow(<HomePage actions={{}} user={{}} history={{}} />);
 
-    const loginInstance = wrapper.find('LoginForm').at(0);
-    const registerInstance = wrapper.find('LoginForm').at(1);
+    const loginForm = wrapper.find('LoginForm').at(0);
+    const registerForm = wrapper.find('LoginForm').at(1);
 
     expect(wrapper.find('LoginForm').length).toBe(2);
-    expect(loginInstance.prop('showRegisterFields')).not.toBeTruthy();
-    expect(registerInstance.prop('showRegisterFields')).toBeTruthy();
+    expect(loginForm.prop('showRegisterFields')).not.toBeTruthy();
+    expect(registerForm.prop('showRegisterFields')).toBeTruthy();
   });
 
   describe('with user.token present in state', () => {
     it('queries self and reroutes to /missions', () => {
       const spy = sinon.spy(HomePage.prototype, 'querySelfAndRedirectToMissions')
+      const history = { push: () => {} };
 
-      shallow(<HomePage actions={actions} user={userWithToken} history={{}} />);
+      shallow(<HomePage actions={actions} user={userWithToken} history={history} />);
 
       expect(spy.called).toBeTruthy();
+      expect(querySelf.mock.calls.length).toBe(1);
     });
   });
 });
