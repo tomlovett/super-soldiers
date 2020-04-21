8bf059bab10bd14485bb30a41353975b06fb5796
Tom Lovett <tomlovett6@gmail.com>
2020-04-21 12:05:54 -0400

    Nuke current build

--- node-api/app.js
+++ node-api/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const createError = require('http-errors');
 
 const app = express();
 app.use(express.json());
@@ -18,21 +17,3 @@ mongoose
 		console.log('Failed to connect to MongoDB...', e);
 		process.exit();
 	});
-
-const soldiersRouter = require('./routes/soldier.route');
-
-// app.use('api/soldiers', soldiersRouter);
-
-app.use((req, res, next) => next(createError(404)) );
-
-// error handler
-app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message  = err.message;
-
-	// render error page
-	res.status(err.status || 500);
-	res.send(err);
-});
-
-module.exports = app;
