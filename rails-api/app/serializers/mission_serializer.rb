0a0cf181df42ba768f62033fda99c741f6c2ab72
Tom Lovett <tomlovett6@gmail.com>
2023-07-18 13:44:29 -0400

    serialize MissionsSoldier with Mission

--- rails-api/app/serializers/mission_serializer.rb
+++ rails-api/app/serializers/mission_serializer.rb
@@ -1,3 +1,8 @@
 class MissionSerializer < ActiveModel::Serializer
-  attributes :id, :name, :user_id
+  attributes :id, :name, :user_id, :soldiers
+
+  def soldiers
+    MissionsSoldier.where(mission_id: object.id)
+                   .joins(:soldier).select(:first_name, :last_name, :nickname, :hits, :misses, :kills)
+  end
 end
