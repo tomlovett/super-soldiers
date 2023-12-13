f1364156f37f21ff9bc5e9ab5c0a123d87d30568
Tom Lovett <tomlovett6@gmail.com>
2023-12-13 12:09:32 -0500

    add Link to /missions

--- next-fe/app/page.tsx
+++ next-fe/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 const Home = () => (
   <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
@@ -10,15 +12,17 @@ const Home = () => (
     <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
       <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
 
-        <div className="relative pl-16">
-          <dt className="text-base font-semibold leading-7">
-            <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-              M
-            </div>
-            View Missions
-          </dt>
-          <dd className="mt-2 text-base leading-7 text-gray-600">Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.</dd>
-        </div>
+        <Link href="/missions">
+          <div className="relative pl-16">
+            <dt className="text-base font-semibold leading-7">
+              <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                M
+              </div>
+              View Missions
+            </dt>
+            <dd className="mt-2 text-base leading-7 text-gray-600">Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.</dd>
+          </div>
+        </Link>
 
         <div className="relative pl-16">
           <dt className="text-base font-semibold leading-7">
