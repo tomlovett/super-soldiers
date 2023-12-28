668845b9e358f2ea78decba787f90d1f9fd840f2
Tom Lovett <tomlovett6@gmail.com>
2023-12-28 12:09:47 -0500

    abstract LinkCard on homepage

--- next-fe/app/page.tsx
+++ next-fe/app/page.tsx
@@ -1,5 +1,26 @@
 import Link from 'next/link'
 
+interface LinkCardProps {
+  href: string
+  headerText: string
+  bodyText: string
+  icon: string
+}
+
+const LinkCard = ({ href, headerText, bodyText, icon }: LinkCardProps) => (
+  <Link href={href}>
+    <div className="relative pl-16">
+      <dt className="text-base font-semibold leading-7">
+        <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+          {icon}
+        </div>
+        {headerText}
+      </dt>
+      <dd className="mt-2 text-base leading-7 text-gray-600">{bodyText}</dd>
+    </div>
+  </Link>
+)
+
 const Home = () => (
   <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <div className="mx-auto max-w-2xl lg:text-center">
@@ -13,33 +34,21 @@ const Home = () => (
 
     <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
       <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-        <Link href="/missions">
-          <div className="relative pl-16">
-            <dt className="text-base font-semibold leading-7">
-              <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                M
-              </div>
-              View Missions
-            </dt>
-            <dd className="mt-2 text-base leading-7 text-gray-600">
-              Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc
-              massa.
-            </dd>
-          </div>
-        </Link>
+        <LinkCard
+          href="/missions"
+          headerText="View Missions"
+          bodyText="Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi."
+          icon="M"
+          key="missions"
+        />
 
-        <div className="relative pl-16">
-          <dt className="text-base font-semibold leading-7">
-            <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-              S
-            </div>
-            View Soldiers
-          </dt>
-          <dd className="mt-2 text-base leading-7 text-gray-600">
-            Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim
-            lacus amet.
-          </dd>
-        </div>
+        <LinkCard
+          href="/soldiers"
+          headerText="View Soldiers"
+          bodyText="Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam amet."
+          icon="S"
+          key="soldiers"
+        />
       </dl>
     </div>
   </main>
