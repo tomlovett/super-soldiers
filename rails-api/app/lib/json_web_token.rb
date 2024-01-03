b3229da4460949515f929efa7b6ff3bed62d6c2a
Tom Lovett <tomlovett6@gmail.com>
2024-01-03 09:57:07 -0500

    remove active model serializers; set auth token expiration to 24 years

--- rails-api/app/lib/json_web_token.rb
+++ rails-api/app/lib/json_web_token.rb
@@ -1,7 +1,8 @@
 class JsonWebToken
   HMAC_SECRET = Rails.application.secrets.secret_key_base
 
-  def self.encode(payload, exp = 24.hours.from_now)
+  # def self.encode(payload, exp = 24.hours.from_now)
+  def self.encode(payload, exp = 24.years.from_now)
     payload[:exp] = exp.to_i
     JWT.encode(payload, HMAC_SECRET)
   end
