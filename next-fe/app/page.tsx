cd827ebded2df2f74d1bd9a57ff7497f8d2222b8
Tom Lovett <tomlovett6@gmail.com>
2024-01-05 11:39:28 -0500

    convert homepage to Material

--- next-fe/app/page.tsx
+++ next-fe/app/page.tsx
@@ -1,44 +1,56 @@
 import Link from 'next/link'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import MilitaryTechIcon from '@mui/icons-material/MilitaryTech'
+import LocalPoliceIcon from '@mui/icons-material/LocalPolice'
 
 interface LinkCardProps {
   href: string
   headerText: string
   bodyText: string
-  icon: string
+  icon: JSX.Element
 }
 
 const LinkCard = ({ href, headerText, bodyText, icon }: LinkCardProps) => (
-  <Link href={href}>
-    <div className="relative pl-16">
-      <dt className="text-base font-semibold leading-7">
-        <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-          {icon}
-        </div>
-        {headerText}
-      </dt>
-      <dd className="mt-2 text-base leading-7 text-gray-600">{bodyText}</dd>
-    </div>
-  </Link>
+  <Grid item xs={12} sm={6} md={6}>
+    <Link href={href}>
+      <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+        <CardContent sx={{ flexGrow: 1 }}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {icon} {headerText}
+          </Typography>
+          <Typography>{bodyText}</Typography>
+        </CardContent>
+      </Card>
+    </Link>
+  </Grid>
 )
 
 const Home = () => (
   <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div className="mx-auto max-w-2xl lg:text-center">
-      <h1 className="mt-2 text-3xl font-semibold tracking-tight sm:text-4xl">Super Soldiers</h1>
-      <h3 className="mt-6 text-lg leading-8 text-indigo-600">The best in modern XCOM mission-tracking</h3>
-      <p className="mt-6 text-lg leading-8 text-gray-600">
+    <Container maxWidth="md">
+      <Typography component="h1" variant="h2" align="center" color="primary" gutterBottom>
+        Super Soldiers
+      </Typography>
+      <Typography variant="h4" align="center" color="primary" gutterBottom>
+        The best in modern XCOM mission-tracking
+      </Typography>
+      <Typography variant="h6" align="center" color="text.secondary" paragraph>
         Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum pulvinar
         et feugiat blandit at. In mi viverra elit nunc.
-      </p>
-    </div>
+      </Typography>
+    </Container>
 
-    <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-      <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+    <Container sx={{ py: 8 }} maxWidth="md">
+      <Grid container spacing={4}>
         <LinkCard
           href="/missions"
           headerText="View Missions"
           bodyText="Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi."
-          icon="M"
+          icon={<MilitaryTechIcon />}
           key="missions"
         />
 
@@ -46,11 +58,11 @@ const Home = () => (
           href="/soldiers"
           headerText="View Soldiers"
           bodyText="Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam amet."
-          icon="S"
+          icon=<LocalPoliceIcon />
           key="soldiers"
         />
-      </dl>
-    </div>
+      </Grid>
+    </Container>
   </main>
 )
 
