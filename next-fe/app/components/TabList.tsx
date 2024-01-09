7b07d5c5d4a1107b136570c37b58477a6f22f325
Tom Lovett <tomlovett6@gmail.com>
2024-01-09 10:43:29 -0500

    fix/refine TabList

--- next-fe/app/components/TabList.tsx
+++ next-fe/app/components/TabList.tsx
@@ -4,23 +4,18 @@ import * as React from 'react'
 import { Box, Tab, Tabs } from '@mui/material'
 
 interface TabPanelProps {
-  activeTab: string
+  isActive: boolean
   elements: React.ReactNode
-  label: string
   index: number
 }
 
-const TabBody = ({ elements, activeTab, label, index, ...other }: TabPanelProps) => (
-  <div
-    role="tabpanel"
-    hidden={activeTab !== label}
-    id={`simple-tabpanel-${index}`}
-    aria-labelledby={`simple-tab-${index}`}
-    {...other}
-  >
-    {label === activeTab && <Box sx={{ p: 3 }}>{elements}</Box>}
+const TabBody = ({ elements, isActive, index, ...other }: TabPanelProps) => (
+  <div role="tabpanel" hidden={!isActive} aria-labelledby={`simple-tab-${index}`} {...other}>
+    {isActive && <Box sx={{ p: 3 }}>{elements}</Box>}
   </div>
 )
+
+const a11yProps = (index: number) => ({ id: `simple-tab-${index}`, 'aria-controls': `simple-tabpanel-${index}` })
 
 interface TabListDataObj {
   label: string
@@ -28,21 +23,21 @@ interface TabListDataObj {
 }
 
 const TabList = ({ dataObjs }: { dataObjs: TabListDataObj[] }) => {
-  const [activeTab, setActiveTab] = React.useState(dataObjs[0].label)
+  const [activeTabIndex, setActiveTabIndex] = React.useState(0)
 
-  const setNewTab = (_event: React.SyntheticEvent, newTabLabel: string) => setActiveTab(newTabLabel)
+  const setNewTab = (_event: React.SyntheticEvent, i: number) => setActiveTabIndex(i)
 
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={activeTab} onChange={setNewTab} aria-label="tabs" centered>
-          {dataObjs.map(({ label }) => (
-            <Tab label={label} key={label} />
+        <Tabs value={activeTabIndex} onChange={setNewTab} aria-label="tabs" centered>
+          {dataObjs.map(({ label }, i) => (
+            <Tab label={label} key={label} {...a11yProps(i)} />
           ))}
         </Tabs>
       </Box>
       {dataObjs.map((thingy, i) => (
-        <TabBody activeTab={activeTab} label={thingy.label} index={i} key={thingy.label} elements={thingy.elements} />
+        <TabBody isActive={activeTabIndex === i} index={i} key={thingy.label} elements={thingy.elements} />
       ))}
     </Box>
   )
